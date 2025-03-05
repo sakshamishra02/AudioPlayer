6165722021f26711d135eeb61dd1a352e763eed6
Saksham Mishra <168401386+sakshamishra02@users.noreply.github.com>
2025-03-05 22:02:45 +0530

    Add files via upload

---
+++ playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "Ram Siya Ram",
+    artist: "jubin",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "Dil-Ibbadt",
+    artist: "KK",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "Born to shine",
+    artist: "Diljit",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "Hass-Hass",
+    artist: "Diljit",
+    img: "img4",
+    audio: "music4"
+  }
+]
